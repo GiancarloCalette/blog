d7ab18f7ce5b7d7f80141123ea0f35b51ddf8a66
Giancarlo Calette <giancarlocalette0@gmail.com>
2023-05-08 16:15:18 -0700

    FSDI 113 Class 1 - Assignment 1

--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,10 @@ from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 from django.views.generic.edit import CreateView
 from .models import Post
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
 
 class PostListView(ListView):
     template_name = "posts/list.html"
@@ -15,14 +18,26 @@ class PostDetailView(DetailView):
 class PostCreateView(LoginRequiredMixin, CreateView):
     template_name = "posts/new.html"
     model = Post
-    fields = ["title", "subtitle", "author", "body"]
+    fields = ["title", "subtitle", "body"]
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = "posts/edit.html"
     model = Post
     fields =  ["title", "subtitle", "body"]
 
-class PostDeleteView(LoginRequiredMixin, DeleteView):
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     template_name = "posts/delete.html"
     model = Post
     success_url = reverse_lazy("list")
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
