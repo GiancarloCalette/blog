7b3f5b678f20a7e4e369bf9b2723bfc321a1d04c
Giancarlo Calette <giancarlocalette0@gmail.com>
2023-05-13 09:28:21 -0700

    Creating files

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (
-    PostListView, 
+    PostListView,
+    DraftPostListView, 
     PostDetailView,
     PostCreateView,
     PostUpdateView, 
@@ -9,6 +10,7 @@ from .views import (
 
 urlpatterns = [
     path("", PostListView.as_view(), name="post_list"),
+    path("drafts/", DraftPostListView.as_view(), name="drafts"),
     path("<int:pk>/", PostDetailView.as_view(), name="post_detail"),
     path("new/", PostCreateView.as_view(), name="post_new"),
     path("<int:pk>/edit/", PostUpdateView.as_view(), name="post_edit"),
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 from django.views.generic.edit import CreateView
-from .models import Post
+from .models import Post, Status
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
@@ -10,6 +10,28 @@ from django.contrib.auth.mixins import (
 class PostListView(ListView):
     template_name = "posts/list.html"
     model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        published_status = Status.objects.get(name="publish")
+        context["post_list"] = Post.objects.filter(
+            status = published_status
+        ).order_by("created_on").reverse()
+        return context
+
+class DraftPostListView(DetailView):
+    template_name = "posts/list.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        draft_status = Status.objects.get(name="draft")
+        context["post_list"] = Post.objects.filter(
+            status = draft_status
+        ).filter(
+            author = self.request.user
+            ).order_by("created_on").reverse()
+        return context
 
 class PostDetailView(DetailView):
     template_name = "posts/detail.html"
@@ -22,13 +44,14 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     
     def form_valid(self, form):
         form.instance.author = self.request.user
+        draft_status = Status.objects.get(name="draft")
+        form.instance.status = draft_status
         return super().form_valid(form)
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = "posts/edit.html"
     model = Post
-    fields =  ["title", "subtitle", "body"]
-
+    fields =  ["title", "subtitle", "body", "status"]
     def test_func(self):
         post = self.get_object()
         return post.author == self.request.user
